3bda1d2b99be54cd8e44c21b2f1b4c9c5ec125aa
Vova <vovan4ksuper@gmail.com>
2021-10-27 17:39:48 +0300

    1

--- ObserverTemplate/Program.cs
+++ ObserverTemplate/Program.cs
@@ -6,18 +6,6 @@ namespace ObserverTemplate
     {
         public static void Main(string[] args)
         {
-            var subject = new Subject();
-            var firstObserver = new Observer();
-            var secondObserver = new Observer();
-
-            subject.Attach(firstObserver);
-            subject.Attach(secondObserver);
-
-            subject.Notify();
-
-            subject.Detach(firstObserver);
-
-            subject.Notify();
         }
     }
 }
